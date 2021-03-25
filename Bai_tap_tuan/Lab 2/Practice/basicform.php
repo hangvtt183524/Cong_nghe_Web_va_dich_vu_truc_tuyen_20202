14d101db38bd6fb1ca86b2b1a76ca7aba2a995d5
bva <hochochoc999@gmail.com>
2021-03-26 00:04:47 +0700

    add practice lab 2

---
+++ Bai_tap_tuan/Lab 2/Practice/basicform.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>A Simple Form</title>
+	</head>
+	<body>
+		<form action="http://fit.hut.edu.vn/~trangntt/course/vp" method=post>	
+			Click submit to start our initial PHP program.
+			<br>
+			<input type="text" name="Name">
+			<br>
+			<input type="SUBMIT" value="Click To Submit">
+			<input type="RESET" value="Erase and Restart">
+		</form>
+	</body>
+</html>

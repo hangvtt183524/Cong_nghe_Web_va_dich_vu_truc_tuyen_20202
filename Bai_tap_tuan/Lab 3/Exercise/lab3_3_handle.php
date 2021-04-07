3c0a57623c6366594717e4539468a6f048560d1b
Le Thi Trung Thu 20183637 <baekhyunee129@gmail.com>
2021-04-07 22:27:50 +0700

    update ex3

---
+++ Bai_tap_tuan/Lab 3/Exercise/lab3_3_handle.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        Hello <?php echo $_POST["name"]?><br><br>
+        Your email is <?php echo $_POST["email"]?><br><br>
+        Your phone number is <?php echo $_POST["phonenumber"]?><br><br>
+        Your idol is <?php echo $_POST["idol"]?> 
+    </body>
+
+</html>

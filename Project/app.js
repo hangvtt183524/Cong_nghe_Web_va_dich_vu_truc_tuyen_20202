0aa3b85c68cdb28d6e038b683ef8440a611b888c
VU THI THU HANG 20183524 <hang.vtt183524@sis.hust.edu.vn>
2021-05-14 17:32:09 +0700

    redirect to another page

--- Project/app.js
+++ Project/app.js
@@ -5,14 +5,19 @@ const path = require('path');
 const app = express();
 
 app.use(express.static('View'));
-app.set('views', './View/html');
+app.set('views', './View');
+
+app.get('/idol_list', (req, res) => {
+    res.sendFile(__dirname + '/View/html/idol_list.html');
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/View/html/welcome_page.html');
 });
 
-app.get('/test', (req, res) => {
-    res.sendFile(__dirname + '/test.html');
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/View/html/login.html');
 });
+
 
 app.listen(3000);

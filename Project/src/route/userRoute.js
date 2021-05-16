7b2d544c0424c15bf79ca4dc97422513d883974c
bva <hochochoc999@gmail.com>
2021-05-16 14:39:00 +0700

    hash password

--- Project/src/route/userRoute.js
+++ Project/src/route/userRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const bcrypt = require('bcrypt');
 
 const User = require('../DB/DB_Model/user');
 
@@ -19,17 +20,29 @@ router.get('/login', (req, res) => {
 router.post('/login', async(req, res, next) => {
     try {
         var username = req.body.username;
-        var password = req.body.password;
 
         var findUser = await User.findOne({username: username});
-
-        if (findUser.password == password) {
-            res.sendFile(path.join(__dirname, '../../View/html', 'idol_list.html'));
-        } else {
-            res.send("wrong pass");
+        
+        if (findUser == null) {
+            return res.send('Cannot find user');
         }
+
+        bcrypt.genSalt(10, (err, salt) => {
+            if (err) throw err;
+            else {
+                bcrypt.compare(req.body.password, findUser.password, (err, isMatch) => {
+                    if (err) throw err;
+                    else if(!isMatch) {
+                        res.send("Pass doesn't match");
+                    }
+                    else {
+                        res.sendFile(path.join(__dirname, '../../View/html', 'idol_list.html'));
+                    }
+                })
+            }
+        })
     } catch (error) {
-        res.status(400).send('invalid');
+        res.send(error);
     }
 })
 
@@ -40,19 +53,27 @@ router.get('/register', (req, res) => {
 router.post('/register', async(req, res) => {
     try {
         var username = req.body.username;
-        var password = req.body.password;
-
         var findUser = await User.findOne({username: username});
 
         if (findUser != null) {
-            return res.send("Invalid username");
-        } else {
-            var newUser = {"username": username, "password": password};
-            User.create(newUser, (err, res) => {
-                if (err) throw err;
-            });
-            return res.send("Register successfully");
+            res.send("Invalid username");
         }
+
+        bcrypt.genSalt(10, (err, salt) => {
+            if (err) throw err;
+            else {
+                bcrypt.hash(req.body.password, salt, (err, hashedPassword) => {
+                    if (err) throw err;
+                    else {
+                        var newUser = {"username": username, "password": hashedPassword};
+                        User.create(newUser, (err, res) => {
+                            if (err) throw err;
+                        });
+                        res.send("Register successfully");
+                    }
+                })
+            }
+        })
     } catch (error) {
         res.status(400).send('invalid');
     }

50d457531671a09bbb8906c5a0b73c2cb0ce68cc
Le Thi Trung Thu 20183637 <baekhyunee129@gmail.com>
2021-05-18 16:53:35 +0700

    fix bug

---
+++ Project/View/javascript/info.js
@@ -0,0 +1,17 @@
+var idolIndex = 1;
+showIdol(idolIndex);
+
+function plusIdol(n) {
+showIdol(idolIndex += n);
+}
+
+function showIdol(n) {
+var i;
+var x = document.getElementsByClassName("idol");
+if (n > x.length) {idolIndex = 1}
+if (n < 1) {idolIndex = x.length}
+for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+}
+x[idolIndex-1].style.display = "block";  
+}

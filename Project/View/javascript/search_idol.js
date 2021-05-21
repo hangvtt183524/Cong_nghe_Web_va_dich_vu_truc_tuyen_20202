624580b75c4e7b26a78af2f57e09f6122fa2de17
VU THI THU HANG 20183524 <hang.vtt183524@sis.hust.edu.vn>
2021-05-21 18:54:07 +0700

    search name using get method

--- Project/View/javascript/search_idol.js
+++ Project/View/javascript/search_idol.js
@@ -1,9 +1,14 @@
-var inputSearch = document.getElementById('search-input');
-var clickSearch = document.getElementById('search-image');
-var form = document.getElementById('form');
+const searchIdol = document.getElementById('search-image');
+const searchInput = document.getElementById('search-input');
 
+searchInput.addEventListener("keyup", event => {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      searchIdol.click();
+    }
+  });
 
-clickSearch.addEventListener('click', () => {
-    document.getElementById('form').submit();
+searchIdol.addEventListener('click', function(e) {
+    window.location.href = "/idol_list?name=" + document.getElementById('search-input').value; 
 });
-  
